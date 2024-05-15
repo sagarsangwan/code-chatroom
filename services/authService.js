1a214b22ccac351e7714dc7cd8be9e41e118d77b
sagar sangwan <iamsagarsangwan@gmail.com>
2024-05-15 18:00:42 +0530

    trying to slove local storage error

--- services/authService.js
+++ services/authService.js
@@ -1,11 +1,17 @@
 "use client"
 import axios from "axios";
 const baseurl = process.env.NEXT_PUBLIC_BASE_URL;
+let access_token = "";
+if (typeof window !== "undefined") {
+    access_token = localStorage.getItem("access_token");
+}
 const instance = axios.create({
     baseURL: baseurl,
     headers: {
-        Authorization: "Bearer " + localStorage.getItem("access_token")
+        Authorization: "Bearer " + access_token
     }
 });
+
+
 
 export default instance;
