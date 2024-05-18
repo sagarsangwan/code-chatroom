beb41735846b9c1a8ec276314c3909ccc44f59a6
sagar sangwan <iamsagarsangwan@gmail.com>
2024-05-18 14:51:43 +0530

    added details to room page

--- app/rooms/[roomId]/page.jsx
+++ app/rooms/[roomId]/page.jsx
@@ -1,15 +1,5 @@
-import { getroom } from "@/lib/data-access/rooms"
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card"
-import { CiEdit } from "react-icons/ci";
-import { FaGithub } from "react-icons/fa";
-import Link from "next/link";
+import { getroom } from "@/lib/data-access/rooms";
+import RoomCard from "@/components/roomCard";
 export default async function RoomPage(props) {
 
     const room = await getroom({ roomid: props.params.roomId });
@@ -19,25 +9,7 @@ export default async function RoomPage(props) {
         <div className="grid grid-cols-4 min-h-screen">
             <div className="col-span-3"></div>
             <div className="col-span-1">
-                <Card>
-                    <CardHeader>
-                        <div className="flex flex-row justify-between">
-                            <CardTitle className="text-xl">{room.name}</CardTitle>
-
-                            <CiEdit size="25px" />
-                        </div>
-                        <CardDescription>{room.description}</CardDescription>
-                    </CardHeader>
-                    <CardContent>
-                        {/* {room.primary_programming_language} */}
-
-                        <Link href={room.github_repo} className="flex items-center gap-2">
-                            Github Repo
-                            <FaGithub />
-                        </Link>
-                    </CardContent>
-
-                </Card>
+                <RoomCard room={room} addbuttonornot={false} />
             </div>
         </div>
     )
