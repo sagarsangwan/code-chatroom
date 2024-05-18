4c829037aa44497b1c7ff8c0edd1e92581cb70ad
sagar sangwan <iamsagarsangwan@gmail.com>
2024-05-18 09:29:59 +0530

    added validation to show data only if available

--- app/page.js
+++ app/page.js
@@ -56,7 +56,6 @@ export default async function Home() {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
 
         {
-          rooms &&
           rooms.map((room) => {
             return <RoomCard key={room.id} room={room} />
           })
